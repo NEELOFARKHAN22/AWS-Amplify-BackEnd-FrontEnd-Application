8114726d22948466800d36ff5af1a425f99400eb
NEELOFARKHAN22 <138312598+NEELOFARKHAN22@users.noreply.github.com>
2024-07-24 17:25:03 +0530

    Update app.php

--- Laravel/config/app.php
+++ Laravel/config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://54.165.35.167'),
+    'url' => env('APP_URL', 'https://neelofar.zapto.org/'),
 
     'asset_url' => env('ASSET_URL'),
 
