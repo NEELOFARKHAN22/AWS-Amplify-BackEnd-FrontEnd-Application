fc08dd6fdef18e3e24834669de1261af7daab70d
NEELOFARKHAN22 <138312598+NEELOFARKHAN22@users.noreply.github.com>
2024-07-24 17:25:50 +0530

    Update cors.php

--- Laravel/config/cors.php
+++ Laravel/config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://54.165.35.167'],
+    'allowed_origins' => ['https://neelofar.zapto.org/'],
 
     'allowed_origins_patterns' => [],
 
